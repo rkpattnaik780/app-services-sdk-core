842ea86b6a0455be702c12511fc83d5d93dcd54d
Jack Delahunt <45426048+jackdelahunt@users.noreply.github.com>
2022-11-21 13:18:08 +0000

    feat: added js sdk

--- .errors/index.js
+++ .errors/index.js
@@ -15,6 +15,24 @@ module.exports ={
             definition: require("./errors_connector_mgmt.json"),
             file: "app-services-sdk-go/connectormgmt/apiv1/error/errors.go"
         }, 
+    },
+    js : {
+        kafka: {
+            definition: require("./errors_kafka_mgmt.json"),
+            file: "app-services-sdk-js/packages/kafka-management-sdk/src/errors.ts"
+        },
+        srs:  {
+            definition: require("./errors_srs_mgmt.json"),
+            file: "app-services-sdk-js/packages/registry-management-sdk/src/errors.ts"
+        },
+        connector: {
+            definition: require("./errors_connector_mgmt.json"),
+            file: "app-services-sdk-js/packages/connector-management-sdk/src/errors.ts"
+        }, 
+        kafkainstance: {
+            definition: require("./errors_kafka_instance.json"),
+            file: "app-services-sdk-js/packages/kafka-instance-sdk/src/errors.ts"
+        }, 
     }
 }
 
--- scripts/errors/generate_errors.js
+++ scripts/errors/generate_errors.js
@@ -2,9 +2,8 @@ const fs = require("fs");
 const { cwd } = require("process");
 const sdks = require(cwd() + "/.errors/index.js");
 
-console.log("Generating errors sdks");
+console.log("===== GO SDK =====");
 
-// will get extended t include more sdks, but process should be the same
 goSdk = sdks["go"]
 
 for (api in goSdk) {
@@ -33,5 +32,47 @@ const (
   stringBuffer += `)`
 
   fs.writeFileSync(cwd() + "/" + apiFileLocation, stringBuffer, { encoding: "utf8" });
-    console.log(`Generated ${api} error definitions`);
+  console.log(`Generated ${api}`);
+}
+
+console.log("===== JS SDK =====");
+
+jsSdk = sdks["js"]
+
+
+for (api in jsSdk) {
+  apiJson = jsSdk[api].definition;
+  apiFileLocation = jsSdk[api].file;
+
+  if (!apiJson || !apiJson.items) {
+    console.log("invalid error file detected", apiJson);
+    exit(1);
+  }
+
+  stringBuffer = `
+/**
+   ${api} error codes
+    \`\`\`ts
+    apiCall.then((data) => {
+        console.log(data?.data.items)
+    }).catch((err) => {
+      if(APIErrorCodes.ERROR_5 == err.response?.data.code) {
+        // Handle error
+      }
+    })
+    \`\`\`
+*/
+export const APIErrorCodes = {
+`;
+
+  apiJson.items.forEach(function (errorType) {
+    stringBuffer += `  /** ${errorType.reason}*/\n`;
+    stringBuffer += `  ERROR_${errorType.id} : "${errorType.code}", \n\n`;
+  });
+
+  stringBuffer += `}`;
+
+  fs.writeFileSync(
+    cwd() + "/" + apiFileLocation, stringBuffer, { encoding: "utf8" });
+  console.log(`Generated ${api}`);
 }
